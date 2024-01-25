a58a7401be735b8296507ec69076b22ba300b3e8
Aishik122 <aishikchatterjee12@gmail.com>
2024-01-25 22:34:23 +0530

    q5 update

--- Flask_assignment1/Q4answer.py
+++ Flask_assignment1/Q4answer.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/home", methods=["GET", "POST"])
+@app.route("/home", methods=["GET","POST"])
 def home():
-    if request.method == "POST":
+    if request.method =="POST":
         name = request.form["name"]
         return render_template("index.html", name=name)
     else:
         return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
