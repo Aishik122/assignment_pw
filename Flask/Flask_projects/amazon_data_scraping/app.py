4dfa11c844c720f9831441e25f2b36f02fa8b7b1
aishik122 <aishikchatterjee12@gmail.com>
2024-03-05 12:47:54 +0530

    arr assignment

--- Flask/Flask_projects/amazon_data_scraping/app.py
+++ Flask/Flask_projects/amazon_data_scraping/app.py
@@ -1,18 +1,28 @@
 from flask import Flask,request , render_template
 import requests
-from src.component import data_ingestion 
+from src.component.data_ingestion import  DataIngestion
+from src.logger import logging
 
 app = Flask(__name__)
 
-@app.route('/home',methods=['GET','POST'])
-def Hellow_world():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        URL= requests.form.get('data-url')
-        df=data_ingestion()
-        final_df=df.web_scrap()
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+@app.route('/process_link', methods=['POST'])
+def process_link():
+    link = request.form['link']
+    # Do something with the link, for example, print it
+    alpha = DataIngestion()
+    alpha.web_scrap(url=link,nums_page=2)
+
+    return render_template('PleaseWait.html')
+
+@app.route('/process_completed')
+def project_complete():
+    return render_template('file generated in artefacts folder')
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
 
