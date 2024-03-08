1e31cac16f8f31690596130a68c864ccc4bc7b41
aishik122 <aishikchatterjee12@gmail.com>
2024-03-08 18:24:15 +0530

    DSA_1

--- Flask/Flask_projects/amazon_data_scraping/app.py
+++ Flask/Flask_projects/amazon_data_scraping/app.py
@@ -1,7 +1,6 @@
-from flask import Flask,request , render_template
-import requests
-from src.component.data_ingestion import  DataIngestion
-from src.logger import logging
+from flask import Flask, request, render_template,redirect
+import pandas as pd
+from src.component.data_ingestion import DataIngestion
 
 app = Flask(__name__)
 
@@ -9,22 +8,29 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/process_link', methods=['POST'])
+@app.route('/process_link', methods=['POST', 'GET'])
 def process_link():
-    link = request.form['link']
-    # Do something with the link, for example, print it
-    alpha = DataIngestion()
-    alpha.web_scrap(url=link,nums_page=2)
+    if request.method == 'POST':
+        link = request.form.get('link')
+        if link:
+            # Process the link and save the data to artifacts folder
+            alpha = DataIngestion()
+            alpha.web_scrap(url=link, nums_page=2)
+            # Redirect to another route or render a template
+            return render_template('/home.html')
+    # If no link is provided or it's a GET request, redirect to the home page
+    return render_template('/home.html')
 
-    return render_template('PleaseWait.html')
 
-@app.route('/process_completed')
-def project_complete():
-    return render_template('file generated in artefacts folder')
+@app.route('/amazon')
+def amazon():
+    amazon_data = read_amazon_data()
+    return render_template('amazon.html', amazon_data=amazon_data)
 
-    
+def read_amazon_data():
+    # Read the CSV file
+    amazon_data = pd.read_csv(r'\artifacts\raw_data.csv')
+    return amazon_data
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
-
-
-
+    app.run(host='0.0.0.0', debug=True)
--- README.md
+++
@@ -1,3 +0,0 @@
-# GitHub repository for Assignment files
-
-
