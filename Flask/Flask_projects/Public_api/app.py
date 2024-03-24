45c30883b5b24fef30d73b52903d3d56d97c8e05
Aishik_c <aishikchatterjee12@gmail.com>
2024-03-24 20:00:22 +0530

    Update app.py

--- Flask/Flask_projects/Public_api/app.py
+++ Flask/Flask_projects/Public_api/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.get('https://api.openweathermap.org/data/2.5/weather?q=kolkata&unit=metric&appid=ee31290b9b49856f1ad681ed5ae94773')
+    response = requests.get('https://api.openweathermap.org/data/2.5/weather?q=kolkata&unit=metric&appid=')
     data = response.json()
 
     # Render the template with weather data
